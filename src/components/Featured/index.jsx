4455f40110215417c326edd4980799e9e6615a34
vdsou <vdsouza@outlook.com>
2021-10-12 11:06:48 -0300

    add featured component

---
+++ src/components/Featured/index.jsx
@@ -0,0 +1,12 @@
+// Featured
+import React from 'react';
+import './Featured.css';
+
+export default function Featured() {
+  return (
+    <section className="featured">
+      <img src="images/robson.svg" alt="RobsonBot" />
+      <h1>Hello, world! I am Robson the bot</h1>
+    </section>
+  );
+}
