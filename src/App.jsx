cd898484e741def8d6d8410dfce49002b360469c
vdsou <vdsouza@outlook.com>
2021-10-12 17:22:01 -0300

    add responsivity to the components so far

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './components/Header';
 import Featured from './components/Featured';
 import Info from './components/Info';
 import './App.css';
 
 function App() {
+  const [userScrollY, setuserScrollY] = useState(false);
+  useEffect(() => {
+    const scrollListener = () => {
+      if (window.scrollY > 10) {
+        setuserScrollY(true);
+      } else {
+        setuserScrollY(false);
+      }
+    };
+    window.addEventListener('scroll', scrollListener);
+    return () => window.removeEventListener('scroll', scrollListener);
+  }, []);
   return (
     <div className="App">
-      <Header />
+      <Header userScroll={userScrollY} />
       <Featured />
       <Info />
     </div>
