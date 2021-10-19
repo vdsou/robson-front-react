aaae3897df1e9fb9e525a890e729e707d0f0562c
vdsou <vdsouza@outlook.com>
2021-10-19 20:11:24 -0300

    add listng in command

--- src/components/Commands/ManageCommands/Command.jsx
+++ src/components/Commands/ManageCommands/Command.jsx
@@ -1,36 +1,67 @@
 // Command: list properties of a command.
-import React, { useContext, useEffect } from 'react';
+/* eslint no-underscore-dangle: 0 */
+import React, { useContext, useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { LayoutContext } from '../../../context/LayoutContext';
 import ListingMenu from '../../ListingMenu';
 import './Command.css';
+import api from '../../../services/api';
 
-export default function Command() {
+export default function Command(props) {
+  const { match } = props;
+  const [idCommand, setIdCommand] = useState('');
+  // const [updatedCommand, setUpdatedCommand] = useState('');
+  const [commandResult, setCommandResult] = useState({});
   const { setShowRobsonStats } = useContext(LayoutContext);
   useEffect(() => {
     setShowRobsonStats(false);
   }, []);
+  useEffect(() => {
+    setIdCommand(match.params.id);
+    if (idCommand) {
+      api
+        .get(`/commands/getcommand/${idCommand}`)
+        .then(({ data }) => {
+          if (data) {
+            setCommandResult(data);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [idCommand]);
+  const handleInput = (event) => {
+    const { value, name } = event.target;
+    setCommandResult({
+      ...commandResult,
+      [name]: value,
+    });
+  };
   return (
     <>
       <ListingMenu />
       <section className="command-list">
-        <h1>bomdia</h1>
+        <h1>{`!${commandResult && commandResult.command}`}</h1>
         <form className="command-form">
-          <label htmlFor="id">
-            ID:
-            <input type="text" value="605dfd9f57c687001fd29af0" id="id" />
-          </label>
-          <label htmlFor="command">
-            Command:
-            <input type="text" value="bomdia" id="command" />
-          </label>
-          <label htmlFor="return">
-            Message return:
-            <input type="text" value="Bom dia VC" id="return" />
-          </label>
-          <label htmlFor="count:">
-            Count:
-            <input type="text" value="" id="count:" />
-          </label>
+          {Object.entries(commandResult).map(
+            (item, index) => item[0] !== '__v' && (
+              // Using index as key here rather than key={Math.random() * 99999}
+              // eslint-disable-next-line react/no-array-index-key
+              <label htmlFor={item[0]} key={index}>
+                {`${item[0]}:`}
+                <input
+                  key={item[0]}
+                  type="text"
+                  defaultValue={item[1] || ''}
+                  name={item[0]}
+                  id={item[0]}
+                  disabled={item[0] === '_id'}
+                  onChange={handleInput}
+                />
+              </label>
+            ),
+          )}
         </form>
         <button type="button">Edit</button>
         <button type="button">Delete</button>
@@ -38,3 +69,6 @@ export default function Command() {
     </>
   );
 }
+Command.propTypes = {
+  props: PropTypes.any,
+}.isRequired;
--- src/components/Featured/index.jsx
+++ src/components/Featured/index.jsx
@@ -5,7 +5,7 @@ import './Featured.css';
 export default function Featured() {
   return (
     <section className="featured">
-      <img src="images/robson.svg" alt="RobsonBot" />
+      <img src="/images/robson.svg" alt="RobsonBot" />
       <h1>Hello, world! I am Robson the bot</h1>
     </section>
   );
